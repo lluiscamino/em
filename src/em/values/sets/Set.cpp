8bd48dd45bbfccb273e62fcb7360ec49c932f67d
Lluís Camino <caminoperez.lluis@gmail.com>
2024-06-29 18:47:37 +0200

    Make Set::intersection return a material set if both sets are material

--- src/em/values/sets/Set.cpp
+++ src/em/values/sets/Set.cpp
@@ -40,8 +40,19 @@ namespace em::values::sets {
 
   std::unique_ptr<Value> Set::intersection(const std::shared_ptr<Value>& other) const {
     using namespace utils::string;
+    if (auto *thisMaterialSet = dynamic_cast<const MaterialSetValue*>(this),
+        *otherMaterialSet = dynamic_cast<const MaterialSetValue*>(other.get());
+        thisMaterialSet && otherMaterialSet) {
+      auto result = std::make_unique<MaterialSetValue>();
+      std::for_each(thisMaterialSet->values().cbegin(), thisMaterialSet->values().cend(), [&](const auto& value) {
+        if (otherMaterialSet->hasElement(value)->isTruthy()) {
+          result->addValue(value);
+        }
+      });
+      return result;
+    }
     return std::make_unique<VirtualSetValue>(std::make_unique<functions::NativeFunction>(
-        stringToWstring(str()) + L" ∪ " + stringToWstring(other->str()),
+        stringToWstring(str()) + L" ∩ " + stringToWstring(other->str()),
         [self = shared_from_this(), other](const auto& args) {
           return std::make_unique<LiteralValue<bool>>(self->hasElement(args[0])->isTruthy() &&
                                                       other->hasElement(args[0])->isTruthy());
---
+++ test/snapshot/EmLang/snapshots/set_intersection/program.em
@@ -0,0 +1,8 @@
+mSet1 := {1, 2, 3, 4}
+mSet2 := {3, 4, 5, 6}
+vResultA := mSet1 ∩ mSet2
+print(vResultA)
+mSet3 := ℝ
+mSet4 := {2, 3}
+vResultB := mSet3 ∩ mSet4
+print(vResultB)
